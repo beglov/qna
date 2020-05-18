d805d3fb00b9542462cd5a51d5c6c3f5ca22c50b
Beglov Sergey <beglov.sergey@yandex.ru>
2020-05-18 14:35:45 +0300

    create answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,6 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
-  validates :question_id, :body, presence: true
+  validates :question_id, presence: true
+  validates :body, presence: {message: "Answer can't be blank"}
 end
