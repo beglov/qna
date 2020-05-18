7c8ff7034880f3bc8d4f9990cc0e74827535df3e
Beglov Sergey <beglov.sergey@yandex.ru>
2020-05-18 15:02:55 +0300

    devise log out

--- spec/features/sign_out_spec.rb
+++ spec/features/sign_out_spec.rb
@@ -6,5 +6,12 @@ feature 'Пользователь может выйти из системы', %q
   Аутентифицированный пользователь
   Должен выйти из системы
 ) do
+  given(:user) { create(:user) }
+
+  scenario 'Аутентифицированный пользователь выходит из системы' do
+    login(user)
+    click_on 'Log out'
+    expect(page).to have_content 'Signed out successfully.'
+  end
 end
 # rubocop:enable Style/RedundantPercentQ
