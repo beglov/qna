7d8ade7c9068fd48e4134c41207326e1c7843a55
Beglov Sergey <beglov.sergey@yandex.ru>
2020-05-24 14:59:59 +0300

    question has many attached files

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ApplicationRecord
   belongs_to :user
   has_many :answers, -> { order('best DESC, created_at') }, dependent: :delete_all
 
-  has_one_attached :file
+  has_many_attached :files
 
   validates :title, :body, presence: true
 end
