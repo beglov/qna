089ea386a2a1447e7fdd7d9487de12c67b986be5
Beglov Sergey <beglov.sergey@yandex.ru>
2020-05-24 17:08:49 +0300

    author can attach files to question when edit it

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -24,17 +24,32 @@ feature 'User can edit his question', %q(
         click_on 'Edit question'
       end
 
-      scenario 'with no errors' do
-        within '#question' do
+      describe 'with valid fields' do
+        background do
           fill_in 'Title', with: 'New Title'
           fill_in 'Body', with: 'New Body'
-          click_on 'Update'
+        end
 
-          expect(page).to_not have_content question.title
-          expect(page).to_not have_content question.body
-          expect(page).to have_content 'New Title'
-          expect(page).to have_content 'New Body'
-          expect(page).to_not have_selector 'textarea'
+        scenario '' do
+          within '#question' do
+            click_on 'Update'
+
+            expect(page).to_not have_content question.title
+            expect(page).to_not have_content question.body
+            expect(page).to have_content 'New Title'
+            expect(page).to have_content 'New Body'
+            expect(page).to_not have_selector 'textarea'
+          end
+        end
+
+        scenario 'and attached files' do
+          within '#question' do
+            attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+            click_on 'Update'
+
+            expect(page).to have_link 'rails_helper.rb'
+            expect(page).to have_link 'spec_helper.rb'
+          end
         end
       end
 
