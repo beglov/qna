2e785fd280e0ee91a9263d51ac292a9f55129f5f
Beglov Sergey <beglov.sergey@yandex.ru>
2020-06-15 00:03:20 +0300

    Github oauth authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :questions, dependent: :nullify
   has_many :answers, dependent: :nullify
   has_many :rewards, dependent: :nullify
+  has_many :authorizations, dependent: :delete_all
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -9,7 +10,20 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:github]
 
   def self.find_for_oauth(auth)
+    authorization = Authorization.find_by(provider: auth.provider, uid: auth.uid.to_s)
+    return  authorization.user if authorization
 
+    email = auth.info[:email]
+    user = User.find_by(email: email)
+    if user
+      user.create_authorization(auth)
+    else
+      password = Devise.friendly_token[0, 20]
+      user = User.create!(email: email, password: password)
+      user.create_authorization(auth)
+    end
+
+    user
   end
 
   def author_of?(resource)
@@ -18,5 +32,9 @@ class User < ApplicationRecord
 
   def voted?(resource)
     resource.votes.exists?(user_id: id)
+  end
+
+  def create_authorization(auth)
+    self.authorizations.create(provider: auth.provider, uid: auth.uid.to_s)
   end
 end
